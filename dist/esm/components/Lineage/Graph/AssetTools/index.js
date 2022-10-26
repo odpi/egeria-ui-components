6633d0df2e0463d057cedea5d5d11fcc83f8c438
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-10-26 15:52:57 +0300

    Update dist and .github
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- dist/esm/components/Lineage/Graph/AssetTools/index.js
+++ dist/esm/components/Lineage/Graph/AssetTools/index.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-import { Button, Grid } from '@mantine/core';
+import { Button, SimpleGrid } from '@mantine/core';
 import { lineageViewsTypesMapping } from 'egeria-js-commons';
 const hasTab = (type, tabName) => {
     if (Object.keys(lineageViewsTypesMapping).includes(type)) {
@@ -11,5 +11,5 @@ const hasTab = (type, tabName) => {
 };
 export function EgeriaAssetTools(props) {
     const { selectedNode } = props;
-    return (_jsx(_Fragment, { children: _jsxs(Grid, Object.assign({ grow: true, gutter: "xs" }, { children: [hasTab(selectedNode.label, 'end-to-end') && _jsx(Grid.Col, Object.assign({ span: 3 }, { children: _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/end-to-end` }, { children: "end-to-end" })) })), hasTab(selectedNode.label, 'vertical-lineage') && _jsx(Grid.Col, Object.assign({ span: 3 }, { children: _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/vertical-lineage` }, { children: "vertical-lineage" })) })), hasTab(selectedNode.label, 'ultimate-source') && _jsx(Grid.Col, Object.assign({ span: 3 }, { children: _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/ultimate-source` }, { children: "ultimate-source" })) })), hasTab(selectedNode.label, 'ultimate-destination') && _jsx(Grid.Col, Object.assign({ span: 3 }, { children: _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/ultimate-destination` }, { children: "ultimate-destination" })) }))] })) }));
+    return (_jsx(_Fragment, { children: _jsxs(SimpleGrid, Object.assign({ cols: 4 }, { children: [hasTab(selectedNode.label, 'end-to-end') && _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/end-to-end` }, { children: "end-to-end" })), hasTab(selectedNode.label, 'vertical-lineage') && _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/vertical-lineage` }, { children: "vertical-lineage" })), hasTab(selectedNode.label, 'ultimate-source') && _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/ultimate-source` }, { children: "ultimate-source" })), hasTab(selectedNode.label, 'ultimate-destination') && _jsx(Button, Object.assign({ component: "a", target: "_blank", href: `/asset-lineage/${selectedNode.id}/ultimate-destination` }, { children: "ultimate-destination" }))] })) }));
 }
