30d150f91da1a762825a2a9ae840951f5d4a04d2
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-10-26 13:51:55 +0300

    Add classifications on <AssetDetails />
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/components/Lineage/Graph/AssetTools/index.tsx
+++ src/components/Lineage/Graph/AssetTools/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid } from '@mantine/core';
+import { Button, SimpleGrid } from '@mantine/core';
 import { lineageViewsTypesMapping } from 'egeria-js-commons';
 
 interface Props {
@@ -17,35 +17,27 @@ export function EgeriaAssetTools(props: Props) {
   const { selectedNode } = props;
 
   return (<>
-    <Grid grow gutter="xs">
-      { hasTab(selectedNode.label, 'end-to-end') && <Grid.Col span={3}>
-        <Button component="a"
+    <SimpleGrid cols={4}>
+      { hasTab(selectedNode.label, 'end-to-end') && <Button component="a"
                 target="_blank"
                 href={ `/asset-lineage/${ selectedNode.id }/end-to-end` }>
           end-to-end
-        </Button>
-      </Grid.Col> }
-      { hasTab(selectedNode.label, 'vertical-lineage') && <Grid.Col span={3}>
-        <Button component="a"
+        </Button> }
+      { hasTab(selectedNode.label, 'vertical-lineage') && <Button component="a"
                 target="_blank"
                 href={ `/asset-lineage/${ selectedNode.id }/vertical-lineage` }>
           vertical-lineage
-        </Button>
-      </Grid.Col> }
-      { hasTab(selectedNode.label, 'ultimate-source') && <Grid.Col span={3}>
-        <Button component="a"
+        </Button> }
+      { hasTab(selectedNode.label, 'ultimate-source') && <Button component="a"
                 target="_blank"
                 href={ `/asset-lineage/${ selectedNode.id }/ultimate-source` }>
           ultimate-source
-        </Button>
-      </Grid.Col> }
-      { hasTab(selectedNode.label, 'ultimate-destination') && <Grid.Col span={3}>
-        <Button component="a"
+        </Button> }
+      { hasTab(selectedNode.label, 'ultimate-destination') && <Button component="a"
                 target="_blank"
                 href={ `/asset-lineage/${ selectedNode.id }/ultimate-destination` }>
           ultimate-destination
-        </Button>
-      </Grid.Col> }
-    </Grid>
+        </Button> }
+    </SimpleGrid>
   </>);
 }
