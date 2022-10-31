e94a1a0d3d5a7dac739318991c4c8a7e5ab86525
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-10-31 16:16:57 +0200

    Remove dist folder and add it to ignore
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- dist/types/components/Assets/Details/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export declare const renderTable: (title: string, object: any) => JSX.Element;
-interface Props {
-    guid?: any;
-}
-export declare function EgeriaAssetDetails(props: Props): JSX.Element;
-export {};
