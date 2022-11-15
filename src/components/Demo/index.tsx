b2e771d2247cc9598b392419efc297f9ae745345
Iliescu Cristian-Mihai <cristianmihaiiliescu@gmail.com>
2022-11-15 18:01:00 +0200

    added print page for lineage; added graph actions component which includes the print button
    Signed-off-by: Iliescu Cristian-Mihai <cristianmihaiiliescu@gmail.com>

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -8,16 +8,18 @@ import { ASSET_CATALOG_PATH, goHome } from '@lfai/egeria-js-commons';
 import { EgeriaApp, EgeriaLogin } from '@lfai/egeria-ui-core';
 
 import './index.scss';
+import { EgeriaLineageGraphPrint } from '../Lineage/Graph/print';
 
 console.log('API_URL', process.env.REACT_APP_API_URL);
 
 export function Demo() {
   return (
-    <div className="container">
+    <div className="custom-full">
       <EgeriaApp single={true} main={
         <Router basename={process.env.REACT_APP_ROOT_PATH}>
           <Routes>
             <Route path={'/asset-lineage/:guid/:lineageType'} element={<EgeriaLineageGraphRouteWrapper  />} />
+            <Route path={'/asset-lineage/:guid/:lineageType/print'} element={<EgeriaLineageGraphPrint  />} />
             <Route path={'/assets/:guid/details'} element={<EgeriaAssetDetails />} />
             <Route path={'/assets/:guid/details/print'} element={<EgeriaAssetDetailsPrint />} />
             <Route path={ ASSET_CATALOG_PATH } element={<EgeriaAssetCatalog  />} />
