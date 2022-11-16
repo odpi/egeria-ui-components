1debb4788a5651d69b82a96d6ac98cf98aec0ddb
GinaIsaia <gina.isaia@ing.com>
2022-11-16 18:11:34 +0100

    replaced simple table with ag-grid for glossary

---
+++ src/components/Glossary/helpers.tsx
@@ -0,0 +1,27 @@
+
+const getGridOptionsGlossary = (columnDefs: any, columnMinWidth?: number) => {
+    return {
+        suppressCellFocus: true,
+        defaultColDef: {
+            sortable: true,
+            resizable: true,
+            minWidth: columnMinWidth ? columnMinWidth : 120
+        },
+
+        columnDefs: [
+            ...columnDefs
+        ],
+
+        onFirstDataRendered: (params: any) => {
+            const allColumnIds: string[] = [];
+            params.columnApi.getColumns()?.forEach((column: any) => {
+                allColumnIds.push(column.getId());
+            });
+            params.columnApi.autoSizeColumns(allColumnIds, true);
+        },
+    };
+}
+
+export {
+    getGridOptionsGlossary
+}
