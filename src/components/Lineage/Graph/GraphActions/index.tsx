0d9b06d044e341c031115a1920106a8a4140a1f2
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-11-25 12:11:45 +0200

    Merge with master intermediate
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
    Signed-off-by: GinaIsaia <gina.isaia@ing.com>

---
+++ src/components/Lineage/Graph/GraphActions/index.tsx
@@ -0,0 +1,29 @@
+import {
+  ActionIcon
+} from '@mantine/core';
+
+import {
+  HappiGraphActions
+} from '@lfai/happi-graph';
+
+import {
+  AiOutlinePrinter
+} from 'react-icons/ai'
+
+interface Props {
+  rawData: any;
+  printUri: string;
+}
+
+export function EgeriaLineageGraphActions (props: Props) {
+  const { rawData, printUri } = props;
+
+  return (
+    <>
+      <HappiGraphActions rawData={{...rawData}}/>
+      <ActionIcon title="Print" variant='subtle' size={35} onClick={() => window.open(printUri)}>
+        <AiOutlinePrinter size={25} />
+      </ActionIcon>
+    </>
+  );
+}
