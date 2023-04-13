848816d14cd7d1e7eab11bd912f359de527d8dd7
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2023-04-13 23:14:11 +0300

    Fix demo app
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/components/Demo/index.tsx
+++ src/components/Demo/index.tsx
@@ -4,8 +4,8 @@ import { EgeriaAssetDetails } from '../Assets/Details';
 import { EgeriaAssetDetailsPrint } from '../Assets/Details/print';
 import { EgeriaGlossary } from '../Glossary';
 import { EgeriaLineageGraphRouteWrapper } from '../Lineage/Graph/RouteWrapper';
-import { ASSET_CATALOG_PATH, goHome, VISIBLE_COMPONENTS } from '@lfai/egeria-js-commons';
-import { EgeriaApp, EgeriaLogin, EgeriaPageNotFound, RequirePermissions } from '@lfai/egeria-ui-core';
+import { ASSET_CATALOG_PATH, eNavigateTo, VISIBLE_COMPONENTS } from '@lfai/egeria-js-commons';
+import { EgeriaApp, EgeriaLogin, EgeriaPageNotFound, RequireAuth, RequirePermissions } from '@lfai/egeria-ui-core';
 
 import './index.scss';
 import { EgeriaLineageGraphPrint } from '../Lineage/Graph/print';
@@ -27,33 +27,71 @@ export function Demo() {
 
   return (
     <div className={fullscreen ? 'custom-full' : 'container'}>
-      <EgeriaApp single={true} main={
-        <Router basename={process.env.REACT_APP_ROOT_PATH}>
+      <Router basename={process.env.REACT_APP_ROOT_PATH}>
+        <EgeriaApp single={true} main={
           <Routes>
-            <Route path={'/asset-lineage/:guid/vertical-lineage'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.VERTICAL_LINEAGE}
-              showAccessDenied={true} element={<EgeriaLineageGraphRouteWrapper  />} /> } />
-            <Route path={'/asset-lineage/:guid/end-to-end'} element={<RequirePermissions component={VISIBLE_COMPONENTS.END_TO_END}
-              showAccessDenied={true} element={<EgeriaLineageGraphRouteWrapper  />} /> } />
-            <Route path={'/asset-lineage/:guid/ultimate-source'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.ULTIMATE_SOURCE}
-              showAccessDenied={true} element={<EgeriaLineageGraphRouteWrapper  />} /> } />
-            <Route path={'/asset-lineage/:guid/ultimate-destination'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.ULTIMATE_DESTINATION}
-              showAccessDenied={true} element={<EgeriaLineageGraphRouteWrapper  />} /> } />
-            <Route path={'/asset-lineage/:guid/:lineageType/print'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.ASSET_LINEAGE_PRINT}
-              showAccessDenied={true} element={<EgeriaLineageGraphPrint  />} /> } />
-            <Route path={'/assets/:guid/details'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.ASSET_CATALOG}
-              showAccessDenied={true} element={<EgeriaAssetDetails  />} /> } />
-            <Route path={'/assets/:guid/details/print'} element={ <RequirePermissions component={VISIBLE_COMPONENTS.ASSETS_DETAILS_PRINT}
-              showAccessDenied={true} element={<EgeriaAssetDetailsPrint  />} /> } />
-            <Route path={ ASSET_CATALOG_PATH } element={<RequirePermissions component={VISIBLE_COMPONENTS.ASSET_CATALOG}
-              showAccessDenied={true} element={<EgeriaAssetCatalog  />} /> }/>
-            <Route path={'/glossary'} element={<RequirePermissions component={VISIBLE_COMPONENTS.GLOSSARY}
-              showAccessDenied={true} element={<EgeriaGlossary columnMinWidth={120}/>} /> } />
-            <Route path={'/login'} element={<EgeriaLogin loginCallback={ goHome }/>} />
-            <Route path={'/*'} element={<EgeriaPageNotFound />} />
+            <Route path={'/asset-lineage/:guid/vertical-lineage'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.VERTICAL_LINEAGE}
+                                  showAccessDenied={true}
+                                  element={<EgeriaLineageGraphRouteWrapper />} />
+            </RequireAuth>   } />
 
-            {/* <Route path={'/asset-lineage/:guid/*'} element={<> HI </>} /> */}
+            <Route path={'/asset-lineage/:guid/end-to-end'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.END_TO_END}
+                                  showAccessDenied={true}
+                                  element={<EgeriaLineageGraphRouteWrapper />} />
+            </RequireAuth> } />
+
+            <Route path={'/asset-lineage/:guid/ultimate-source'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ULTIMATE_SOURCE}
+                                  showAccessDenied={true}
+                                  element={<EgeriaLineageGraphRouteWrapper />} />
+            </RequireAuth> } />
+
+            <Route path={'/asset-lineage/:guid/ultimate-destination'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ULTIMATE_DESTINATION}
+                                  showAccessDenied={true}
+                                  element={<EgeriaLineageGraphRouteWrapper />} />
+            </RequireAuth> } />
+
+            <Route path={'/asset-lineage/:guid/:lineageType/print'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ASSET_LINEAGE_PRINT}
+                                  showAccessDenied={true}
+                                  element={<EgeriaLineageGraphPrint />} />
+            </RequireAuth> } />
+
+            <Route path={'/assets/:guid/details'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ASSET_CATALOG}
+                                  showAccessDenied={true}
+                                  element={<EgeriaAssetDetails  />} />
+            </RequireAuth> } />
+
+            <Route path={'/assets/:guid/details/print'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ASSETS_DETAILS_PRINT}
+                                  showAccessDenied={true}
+                                  element={<EgeriaAssetDetailsPrint  />} />
+            </RequireAuth> } />
+
+            <Route path={ ASSET_CATALOG_PATH } element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.ASSET_CATALOG}
+                                  showAccessDenied={true}
+                                  element={<EgeriaAssetCatalog  />} />
+            </RequireAuth> }/>
+
+            <Route path={'/glossary'} element={<RequireAuth>
+              <RequirePermissions component={VISIBLE_COMPONENTS.GLOSSARY}
+                                  showAccessDenied={true}
+                                  element={<EgeriaGlossary columnMinWidth={120}/>} />
+            </RequireAuth> } />
+
+            <Route path={'/login'} element={
+              <EgeriaLogin loginCallback={ () => eNavigateTo('/') }/>
+            } />
+
+            <Route path={'/*'} element={<EgeriaPageNotFound />} />
           </Routes>
-        </Router> } />
+        } />
+      </Router>
     </div>
   );
 }
